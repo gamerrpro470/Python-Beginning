4cce58c8d152a738ea316bb18f227c2326de325d
gamerrpro470 <gamerrpro470@gmail.com>
2026-08-12 19:27:40 +0500

    tuple

---
+++ class14.py
@@ -0,0 +1,21 @@
+# #list
+# numbers = [ 1 , 2, 3 ,4 ,5, 6]
+# print(type(numbers))
+# percent = [ 1.03 , 9.001 , 79.43]
+# print(type(percent))
+# snacks = [ "lays chips" ,  "m&ms" , "chocolate cookies" , "coca cola"]
+# print(type(snacks))
+# group = [ 1 , 2 ,3 ,4 , "ahmed" , "afaan" , "isaam", 0.001 , 11.3 , 9981.31 , True , False , False ,True]
+# print(type(group))
+# #tuple 
+# numbers = (1 , 2 ,3 ,4 ,5,)
+# print(type(numbers))
+# percent = (01.033 , 0.77 , 943.9992,)
+# print(type(percent))
+# names = ("isaam" , "ali" , "afaan" , "ahmed",)
+# print(type(names))
+# group = ( 1 , 2 ,3, 4 ,6, 8 , "ali" , "arham" , "abdullah" , 09.88 , 887.777 , 66.00001 , True , False ,[12 ,4, 85 ,86] ,)
+# print(type(group))
+
+
+
