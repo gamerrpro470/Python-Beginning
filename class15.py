e3e02edf4fd3b1c46e82fabe6bbac1c68f99471b
gamerrpro470 <gamerrpro470@gmail.com>
2026-08-13 19:01:35 +0500

    tuple

---
+++ class15.py
@@ -0,0 +1,19 @@
+#tuple
+# numbers = (1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10,)
+# print(numbers[6])
+
+# number = int(input("enter your number:"))
+# for i in range (4):
+#     number = int(input("enter your number: "))
+
+#city names
+# cities = ( "islamabad", "lahore" , "punjab" , "balochistan",)
+# print(cities)
+
+# tuple = (10 , 20 , 30 , 40 , 50,)
+# print(tuple[-1])
+
+# tuplee= ("a" , "b" , "c" , "d" , "e")
+# print(tuplee[1 : 4])
+
+
