f0f0ecd7bdde8bb3ffb14dfaf2d610aebfbbed98
gamerrpro470 <gamerrpro470@gmail.com>
2026-08-21 19:48:13 +0500

    module 2

---
+++ module2.py
@@ -0,0 +1,40 @@
+# # coding exercise
+# # Q1
+# num1 = float(input("enetr num1 :"))
+# num2 = float(input("enter num2 :"))
+# addition = num1 + num2
+# subtraction = num1 - num2
+# multiplication = num1 * num2
+# division = num1 / num2
+# print(addition)
+# print(subtraction)
+# print(multiplication)
+# print(division)
+
+# # Q2
+# num = int(input("enter number"))
+# if num % 2 == 0:
+#     print("number is even")
+# else :
+#     print("number is odd")
+
+# # Q3
+# item_price = float(input("enter item's price :"))
+# quantity = int(input("enter item's quantity :"))
+# total_bill = item_price * quantity
+# print(total_bill)
+
+# # Q5
+# student_marks = float(input('enter your marks :'))
+# if student_marks >= 40 and student_marks <100 :
+#     print("passed")
+# elif student_marks <= 40 and student_marks >= 0 :
+#     print("failed")
+# else :
+#     print("invalid marks")
+
+
+# assignment operator
+f = 6
+f**= 4
+print(f)
