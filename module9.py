0d9e8d15cb4d1f6762a812e17d88b1674f0610dd
gamerrpro470 <gamerrpro470@gmail.com>
2026-08-31 19:38:44 +0500

    module 9

---
+++ module9.py
@@ -0,0 +1,32 @@
+# dictionary
+
+# coding exercises
+# Q1
+new_fruits = { "watermelon" : 150 , "raspberry" : 350 , "kiwi" : 400 }
+print(new_fruits)
+
+# Q2
+new_fruits["avocado"] = 300
+new_fruits["kiwi"] = 250
+print(new_fruits)
+
+# Q3
+print(new_fruits.get("grapes"))
+
+# Q4
+print(new_fruits.keys())
+print(new_fruits.values())
+
+# Q5
+for fruits in new_fruits :
+    print(new_fruits.items()) 
+
+
+# Q
+# football_players = { "Spain" : "lamine Yamal" , "Portugal" : "Cristiano Ronaldo" , "France" : "Kylian Mbappé" , "Argentina" : "Lionel Messi" }
+# print(football_players["Portugal"])
+
+
+
+
+
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-student_marks = float(input("enter your marks :"))
-if student_marks >= 40 and student_marks < 101:
-    print("you have passed the exam")
-elif student_marks < 40 and student_marks <=0:
-    print("you have failed the exam")
