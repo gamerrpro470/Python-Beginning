39b661416b9d9edb310e8e503764de4a61bb8d8e
gamerrpro470 <gamerrpro470@gmail.com>
2026-09-03 19:34:10 +0500

    module9

--- module9.py
+++ module9.py
@@ -82,44 +82,56 @@
 # print(Data["student2"]["name"])
 
 # Q8
-dictionary = { "rtx 4090" : 800000 , "rtx 4080" : 650000 , "rtx 4070" : 500000 }
-dictionary.clear()
-print(dictionary)
+# dictionary = { "rtx 4090" : 800000 , "rtx 4080" : 650000 , "rtx 4070" : 500000 }
+# dictionary.clear()
+# print(dictionary)
 
 
+# Mini Assignment
+# employee = { "name" : "ahmed" , "department" : "IT" , "salary" : 80000 }
+# bonus_value = employee.get("bonus" , 0 )
+# employee.update({"salary" : 90000 , "experience" : "4 years"})
+# for keys , values in employee.items():
+#     print(f"{keys} : {values}")
+# removing_value = employee.pop("experience")
+# print(employee)
 
+# Mini Project
+students = { "roll_no1" : {"name" : "Ali" , "age" : 20 , "marks" : 80} ,
+             "roll_no2" : {"name" : "Ahmed" , "age" : 22 , "marks" : 90} ,
+             "roll_no3" : {"name" : "Abdullah" , "age" : 21 , "marks" : 85} 
+            }
+print(f"taking_input_from_user \n")
 
+roll_no = input("Enter the roll number :")
+name = input("Enter the name :")
+age = int(input("Enter the age :"))
+marks = int(input("Enter the marks :"))
 
+print(f"updating_the_dictionary\n")
+students.update({roll_no: {"name" : name , "age" : age , "marks" : marks}})
+print(students)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(f"searching_the_dictionary\n")
+roll_number = input("Enter the roll number to see details :")
+if roll_number in students:
+    print(students[roll_number])
+elif roll_number not in students:
+    print("roll number not found")
+else:
+    print("invalid roll number")
+print(f"updating_the_dictionary\n")
+updating_marks = input("Enter the roll number to update marks :")
+if updating_marks in students:
+    update_marks = input("Enter the new marks :")
+    students[updating_marks].update({"marks" : update_marks})
+    print(students[updating_marks])
+else :
+    print("roll number was not found")
+for keys , values in students.items():
+    print(f"{keys} --> {values}")
+students.pop("roll_no2")
+print(students)
 
 
 
