5413e46896fb463d6962dc891d3c8c5c431d95af
gamerrpro470 <gamerrpro470@gmail.com>
2026-09-04 19:52:52 +0500

    class17

---
+++ class17.py
@@ -0,0 +1,22 @@
+# a = float(input("Enter first number : "))
+# b = float(input("Enter second number : "))
+# def addition(a = 5 , b = 10):
+#      print(a + b)
+# addition()
+# addition(20 , 30)
+
+# addition(a , b)
+# def welcome():
+#     print("Welcome to Python")
+# welcome()
+
+# def student_info(name, age):
+#     print(name , age)
+# student_info("Ali" , 20)
+
+# def square(number):
+#     print(number ** 2)
+# square(10)
+
+
+
