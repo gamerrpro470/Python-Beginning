ec96aad270304b0cc7aac9454e2855894213481f
gamerrpro470 <gamerrpro470@gmail.com>
2026-09-07 16:51:43 +0500

    homework

--- class17.py
+++ class17.py
@@ -18,5 +18,65 @@
 #     print(number ** 2)
 # square(10)
 
+# Q1
+# def multiply(a, b):
+#     result = a * b
+#     print(result)
+# multiply(5, 6)
+
+# Q2
+# def square(number):
+#     result = number * number
+#     print(result)
+# square(7)
+
+# Q3
+# def student_details(name, age, course):
+#     print("Student Name:", name)
+#     print("Age:", age)
+#     print("Course:", course)
+# student_details("Ahmed", 21, "Computer Science")
+
+# Q4
+# def calculate_marks(subject1, subject2, subject3):
+#     total = subject1 + subject2 + subject3
+#     print("Total Marks:", total)
+# calculate_marks(85, 92, 78)
+
+# Q5
+# def introduce(name, city):
+#     print("Student Name:", name)
+#     print("City:", city)
+# introduce("Ali", "Karachi")
+
+# Q6
+# def calculate_area(length, width):
+#     area = length * width
+#     print("area :", area)
+# calculate_area(10, 5)
+
+# Q7
+# def welcome_student(name, course):
+#     print(f"Welcome {name} to the {course} course!")
+# welcome_student("David", "Data Science")
+
+# Q8
+# def calculate_total(price, quantity):
+#     total = price * quantity
+#     print(f"Total Amount: {total}")
+# calculate_total(250, 4)
+
+# Q9
+# def student_result(name, marks):
+#     print(f"Student Name: {name}")
+#     print(f"Marks Obtained: {marks}")
+# student_result("Isaam", 95)
+
+# Q10
+# def calculate_age(name, birth_year):
+#     print("Student Name:", name)
+#     print("Birth Year:", birth_year)
+# calculate_age("Jimmy", 2001)
+
 
 
