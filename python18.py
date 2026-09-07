bdb518dab421fbd03f034e7905c741f3db78adba
gamerrpro470 <gamerrpro470@gmail.com>
2026-09-07 18:53:52 +0500

    class18

---
+++ python18.py
@@ -0,0 +1,46 @@
+# functions
+# def area_of_rectangle(length, width):
+#     return length * width
+# area = area_of_rectangle(10, 5)
+# print(area)
+
+# Q1
+# def calculate_bill(price, quantity=1):
+#     return price * quantity
+# bill_with_only_price = calculate_bill(500)
+# print(f"Total bill with only price: {bill_with_only_price}")
+# bill_with_quantity = calculate_bill(500, 3)
+# print(f"Total bill with price and quantity (3): {bill_with_quantity}")
+
+# Q2
+# def area(length, width):
+#     return length * width
+# rectangle_area = area(10, 5)
+# print(f"Rectangle total area: {rectangle_area}")
+
+# Q3
+# def subtract(a, b):
+#     return a - b
+# result = subtract(15, 7)
+# print(f"Difference: {result}")
+
+# Q4
+# def show_numbers(*numbers):
+#     for num in numbers:
+#         print(num)
+# show_numbers(10, 20, 30)
+
+# Q5
+# def add_numbers(*numbers):
+#     return sum(numbers)
+# total_sum = add_numbers(5, 10, 15, 20)
+# print(f"Total Sum: {total_sum}")
+
+
+
+
+
+
+
+
+
