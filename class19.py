797bdfc1fda7583b54b51065a6e4fb927c367e71
gamerrpro470 <gamerrpro470@gmail.com>
2026-09-08 19:03:05 +0500

    module 10

---
+++ class19.py
@@ -0,0 +1,31 @@
+# recursion
+# def countdown(n):
+#     if n == 0:
+#      return
+#     print(n)
+#     countdown(n - 2)
+
+# countdown(20)
+
+# Q1
+# def student_info ():
+#     name = "Duaa"
+#     print(name)
+
+# student_info()
+
+# Q2
+# school = "MHK Academy"
+# def show_school():
+#     print(school)
+
+# show_school()
+
+# Q3
+# def print_even (n):
+#     if n == 0:
+#         return
+#     print(n)
+#     print_even(n - 2)
+
+# print_even(10)
